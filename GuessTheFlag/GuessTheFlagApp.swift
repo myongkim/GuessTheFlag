d78e4d7a9d2151ee1d9f0fa66f5fd3bc716297b2
Isaac Kim <myong328@gmail.com>
2022-03-31 06:51:16 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Isaac Kim on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
