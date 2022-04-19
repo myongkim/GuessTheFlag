a8d201db042067449482b8df2ec7b018d088dd7a
Isaac Kim <myong328@gmail.com>
2022-04-19 10:17:36 -0700

    accessibility label has been added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -28,6 +28,20 @@ struct ContentView: View {
     
     static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     var body: some View {
         ZStack{
             RadialGradient(stops: [
@@ -61,6 +75,8 @@ struct ContentView: View {
                         } label: {
                            FlagImage(name: countries[number])
                                 
+
+                                
                             
                         }
                         .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
@@ -70,6 +86,7 @@ struct ContentView: View {
                         .blur(radius: selectedFlag == -1 || selectedFlag == number ? 1 : 5)
 
                         .animation(.default, value: selectedFlag)
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         
                         
                     }
